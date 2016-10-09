6687195f4c15f06ba1ffcc94adfe36765f8726ea
Bolza <stefano.sergio@gmail.com>
2016-10-09 19:25:38 +0100

    score and time left

--- Assets/Scripts/BallCollide.cs
+++ Assets/Scripts/BallCollide.cs
@@ -2,10 +2,15 @@
 using System.Collections;
 
 public class BallCollide : MonoBehaviour {
-
+	GameController game;
+	public int points = 1;
+	void Start() {
+		game = GameObject.Find("GameLevelController").GetComponent<GameController>();
+	}
 	void OnTriggerEnter2D(Collider2D other){
 		
 		if (other.gameObject.tag == "Hat") {
+			game.addScore (points);
 			Destroy (this.gameObject);
 		} else if (other.gameObject.name == "LevelBoundary") {
 			Destroy (this.gameObject);
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,24 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameController : MonoBehaviour {
 	Vector3 screen;
 	public GameObject BowlingBall;
+	public float timeLeft;
+	public Text timerText; 
+	public Text scoreText; 
+	int playerScore = 0;
+
 	void Start () {
 		var v3 = new Vector3 (Screen.width, Screen.height, Camera.main.nearClipPlane);
 		screen = Camera.main.ScreenToWorldPoint (v3);
 		StartCoroutine( Spawn () );
 	}
+
 	public IEnumerator Spawn() {
 		yield return new WaitForSeconds (2);
-		while (true) {
+		while (timeLeft > 0) {
 			float x = Random.Range (-screen.x, screen.x);
 			Vector3 sposition = new Vector3 (x, screen.y+2, screen.z);
 			GameObject ball = (GameObject)Instantiate (BowlingBall, sposition , Quaternion.identity);
-			Vector3 force = new Vector3(Random.Range (-300, 300), 0, 0);
-			ball.GetComponent<Rigidbody2D> ().AddForce (force);
+//			Vector3 force = new Vector3(Random.Range (-300, 300), 0, 0);
+//			ball.GetComponent<Rigidbody2D> ().AddForce (force);
 			yield return new WaitForSeconds (Random.Range (1, 2));
 		}
 	}
 
+	void FixedUpdate() {
+		timeLeft -= Time.deltaTime;
+		timerText.text = "Time Left: " + Mathf.Max(0, Mathf.Round(timeLeft));
+	}
+
+	public void addScore(int score) {
+		playerScore += score;
+		scoreText.text = "Score: " + playerScore;
+	}
 }
