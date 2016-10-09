d8645eded8a7926bec58977bf99d9a713f9cba24
Bolza <stefano.sergio@gmail.com>
2016-10-09 16:27:12 +0100

    hat controlled by mouse correctly

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,30 +2,23 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	float maxWidth;
-	// Use this for initialization
+	float originalY = 0f;
+
 	void Start () {
-		Vector3 uc = new Vector3 (Screen.width, Screen.height, 10);
-		Vector3 raw = Camera.main.ScreenToWorldPoint (uc);
-		maxWidth = raw.x;
+		originalY = transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-//		var clip = Camera.main.nearClipPlane ();
-//		float x = Input.GetAxis ("Horizontal") * Time.deltaTime;
-		float mousex = Input.GetAxis ("MouseX");
-		float mousey = Input.GetAxis ("MouseY");
-		Vector3 mouse = new Vector3 (mousex, mousey, 1);
-		Vector3 world = Camera.main.ScreenToWorldPoint (this.transform.position);
-//		Debug.logger.Log ("mouse",mousex );
-//		Debug.logger.Log ("world");
-		Rigidbody2D cmp = GetComponent<Rigidbody2D>();
-//		this.transform.position = mouse;
-		float clampedx = Mathf.Clamp (mouse.x, -Screen.width, Screen.width);
-//		this.transform.position -9 / +9
-		cmp.transform.position = Camera.main.ScreenToWorldPoint (new Vector3(Input.mousePosition.x, Input.mousePosition.y, 12));
-		Debug.logger.Log ("pos",cmp.transform.position );
-		cmp.transform.Translate (clampedx, 0f, 0f);
+//		Debug.logger.Log("finger")
+//		float clampedx = Mathf.Clamp (mouse.x, size, Screen.width-size);
+		MouseControl();
+	}
+
+	void MouseControl() {
+		var v3 = Input.mousePosition;
+		v3.z = Camera.main.nearClipPlane;
+		v3 = Camera.main.ScreenToWorldPoint(v3);
+		transform.position = new Vector3(v3.x, originalY, v3.z);
 	}
 }
