ea6680b4b48d89c0afc65f514e6b9e72e0d9df92
Bolza <stefano.sergio@gmail.com>
2016-10-09 22:11:18 +0100

    refact code and last touches

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,29 +8,52 @@ public class GameController : MonoBehaviour {
 	public float timeLeft;
 	public Text timerText; 
 	public Text scoreText; 
+	public GameObject gameOverLabel;
+	public GameObject restartButton;
+	public GameObject mainPlayer;
+
 	int playerScore = 0;
 
 	void Start () {
 		var v3 = new Vector3 (Screen.width, Screen.height, Camera.main.nearClipPlane);
 		screen = Camera.main.ScreenToWorldPoint (v3);
+		mainPlayer.SetActive (true);
+		gameOverLabel.SetActive (false);
+		restartButton.SetActive (false);
 		StartCoroutine( Spawn () );
 	}
 
 	public IEnumerator Spawn() {
 		yield return new WaitForSeconds (2);
 		while (timeLeft > 0) {
-			float x = Random.Range (-screen.x, screen.x);
-			Vector3 sposition = new Vector3 (x, screen.y+2, screen.z);
-			GameObject ball = (GameObject)Instantiate (BowlingBall, sposition , Quaternion.identity);
-//			Vector3 force = new Vector3(Random.Range (-300, 300), 0, 0);
-//			ball.GetComponent<Rigidbody2D> ().AddForce (force);
-			yield return new WaitForSeconds (Random.Range (1, 2));
+			SpawnBall ();
+			yield return new WaitForSeconds (Random.Range (0.5f, 1.5f));
+		}
+		if (timeLeft <= 0) {
+			GameOver ();
 		}
 	}
 
+	void SpawnBall() {
+		float x = Random.Range (-screen.x, screen.x);
+		Vector3 sposition = new Vector3 (x, screen.y+2, screen.z);
+		GameObject ball = (GameObject)Instantiate (BowlingBall, sposition , Quaternion.identity);
+	}
+
 	void FixedUpdate() {
+		timeUpdater ();
+	}
+
+	void timeUpdater() {
 		timeLeft -= Time.deltaTime;
 		timerText.text = "Time Left: " + Mathf.Max(0, Mathf.Round(timeLeft));
+	}
+
+	void GameOver() {
+		gameOverLabel.SetActive (true);
+		restartButton.SetActive (true);
+		mainPlayer.SetActive (false);
+
 	}
 
 	public void addScore(int score) {
---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class RestartGame : MonoBehaviour {
+
+	// Use this for initialization
+	public void Restart() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+	}
+}
